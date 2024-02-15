c03389f947a47a86ed07365eb297df836887043c
philteigne <100657049+philteigne@users.noreply.github.com>
2024-02-15 12:04:14 -0500

    implement PotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  return(
+    <div>
+      <img src={props.sampleDataForPhotoListItem.imageSource}></img>
+      <div>
+        <img src={props.sampleDataForPhotoListItem.profile}></img>
+        <div>
+          <h1>{props.sampleDataForPhotoListItem.username}</h1>
+          <h2>{props.sampleDataForPhotoListItem.location.city}, {props.sampleDataForPhotoListItem.location.country}</h2>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default PhotoListItem;
