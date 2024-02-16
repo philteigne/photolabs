374af797175fd6eac948b703d35be6f7044dddcf
philteigne <100657049+philteigne@users.noreply.github.com>
2024-02-15 19:31:26 -0500

    display 3 photo listings

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,24 +5,38 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   /* Insert React */
   const {sampleDataForPhotoListItem} = props
+
+  const photos = new Array(sampleDataForPhotoListItem, sampleDataForPhotoListItem, sampleDataForPhotoListItem)
+ 
+  console.log(photos);
+  const listOfPhotos = photos.map((photo) => {
   return(
-    <div className="photo-list__item">
+    <div className="photo-list__item" key={photo.id}>
+    <img
+    src={photo.imageSource}
+    alt="Selected photograph"
+    className="photo-list__image"
+    ></img> 
+    <div className="photo-list__user-details">
       <img
-      src={sampleDataForPhotoListItem.imageSource}
-      alt="Selected photograph"
-      className="photo-list__image"
-      ></img> 
-      <div className="photo-list__user-details">
-        <img
-        src={sampleDataForPhotoListItem.profile}
-        alt="Photographer profile image"
-        className="photo-list__user-profile"
-        ></img>
-        <div className="photo-list__user-info">
-          <p>{sampleDataForPhotoListItem.username}</p>
-          <p className="photo-list__user-location">{sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}</p>
-        </div>
+      src={photo.profile}
+      alt="Photographer profile image"
+      className="photo-list__user-profile"
+      ></img>
+      <div className="photo-list__user-info">
+        <p>{photo.username}</p>
+        <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
       </div>
+    </div>
+  </div>
+  )
+  })
+
+  console.log(listOfPhotos)
+
+  return(
+    <div className="photo-list">
+      {listOfPhotos}
     </div>
   )
 };
