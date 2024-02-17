62a7e7b2e6a904d4992b1cb1f922137f9f81a50d
philteigne <100657049+philteigne@users.noreply.github.com>
2024-02-16 19:22:58 -0500

    implement mock data an cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
-import PhotoList from 'components/PhotoList';
-import TopNavigationBar from 'components/TopNavigationBar';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
-
-
-// const photos = new Array(sampleDataForPhotoListItem, sampleDataForPhotoListItem, sampleDataForPhotoListItem)
 
 // Note: Rendering a single component to build components in isolation
 const App = () => (
